b0637a9eed5fbccacc65dd3dcefa03a4a7b16215
viyfe <lianviy3095@gmail.com>
2026-03-29 20:17:59 +0800

    Update rainviewer.js

--- rainviewer.js
+++ rainviewer.js
@@ -1,31 +1,38 @@
 /*
-RainViewer Pro Unlock - 2026 专用版
-基于接口: /mobile/purchases/ios/actual
+RainViewer Pro Unlock - 2026 最终方案
+支持接口: /mobile/purchases/ios/actual 以及 /v2/profile
 */
 
-let obj = JSON.parse($response.body);
+let body = $response.body;
+if (!body) $done({});
 
-if (obj.data) {
-    // 基础购买状态
-    obj.data.purchased = true;
-    obj.data.is_expired = false;
-    obj.data.is_cancelled = false;
-    obj.data.is_trial = true;
-    
-    // 设置一个遥远的过期时间 (2099年)
-    obj.data.expiration = 4070908800; 
-    
-    // 注入所有可能的 Pro 产品 ID
-    obj.data.products = [
-        "PREMIUM_FEATURES_3_1YEAR",
-        "PRO_1YEAR",
-        "premium_features_v2",
-        "no_ads"
-    ];
-    
-    // 其他关键字段
-    obj.data.plan_id = "PRO_1YEAR";
-    obj.data.has_orders = true;
+let obj = JSON.parse(body);
+
+// 方案 A: 针对你抓到的 /actual 接口
+if ($request.url.indexOf("/actual") !== -1) {
+    obj.data = {
+        "id": "2000000476197081",
+        "purchased": true,
+        "type": 1, // 提升类型等级
+        "products": ["PRO_1YEAR", "premium_features_v2", "PREMIUM_FEATURES_3_1YEAR"],
+        "expiration": 4070908800,
+        "has_orders": true,
+        "is_trial": false,
+        "is_expired": false,
+        "is_grace": false,
+        "is_cancelled": false,
+        "is_test": false,
+        "duration": "YEAR",
+        "plan_id": "PRO_1YEAR"
+    };
 }
 
-$done({body: JSON.stringify(obj)});
+// 方案 B: 针对备用的 /v2/profile 接口
+if ($request.url.indexOf("/v2/profile") !== -1) {
+    obj.is_premium = true;
+    obj.premium_until = 4070908800;
+    obj.ads_disabled = true;
+    obj.features = ["all", "high_res", "fast_update"];
+}
+
+$done({ body: JSON.stringify(obj) });
