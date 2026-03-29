f47d62b8dcd2f89fa7f07817697909a468b3c7e5
viyfe <lianviy3095@gmail.com>
2026-03-29 20:14:04 +0800

    Update rainviewer.js

--- rainviewer.js
+++ rainviewer.js
@@ -1,22 +1,31 @@
 /*
-RainViewer Pro Unlock - 增强版
+RainViewer Pro Unlock - 2026 专用版
+基于接口: /mobile/purchases/ios/actual
 */
 
-let body = $response.body;
-let obj = JSON.parse(body);
+let obj = JSON.parse($response.body);
 
-// 核心解锁逻辑：强制赋予会员属性
-obj.is_premium = true;
-obj.premium = true;
-obj.ads_disabled = true;
-obj.subscription_type = "premium";
-obj.premium_until = 4070908800; // 2099年
-obj.features = ["all", "no_ads", "high_res", "fast_update"];
-
-// 针对部分版本可能存在的嵌套结构
 if (obj.data) {
-    obj.data.is_premium = true;
-    obj.data.premium_until = 4070908800;
+    // 基础购买状态
+    obj.data.purchased = true;
+    obj.data.is_expired = false;
+    obj.data.is_cancelled = false;
+    obj.data.is_trial = true;
+    
+    // 设置一个遥远的过期时间 (2099年)
+    obj.data.expiration = 4070908800; 
+    
+    // 注入所有可能的 Pro 产品 ID
+    obj.data.products = [
+        "PREMIUM_FEATURES_3_1YEAR",
+        "PRO_1YEAR",
+        "premium_features_v2",
+        "no_ads"
+    ];
+    
+    // 其他关键字段
+    obj.data.plan_id = "PRO_1YEAR";
+    obj.data.has_orders = true;
 }
 
 $done({body: JSON.stringify(obj)});
