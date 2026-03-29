96f1e24d17e2d592de36d4aa6351fef0ef1ba817
viyfe <lianviy3095@gmail.com>
2026-03-29 20:06:36 +0800

    Update rainviewer.js

--- rainviewer.js
+++ rainviewer.js
@@ -1,14 +1,22 @@
 /*
-RainViewer Unlock Pro
+RainViewer Pro Unlock - 增强版
 */
 
-let obj = JSON.parse($request.body);
-// 模拟 Pro 账户返回的数据
-obj = {
-  "is_premium": true,
-  "premium_until": 4070908800, // 2099年
-  "ads_disabled": true,
-  "features": ["all"]
-};
+let body = $response.body;
+let obj = JSON.parse(body);
+
+// 核心解锁逻辑：强制赋予会员属性
+obj.is_premium = true;
+obj.premium = true;
+obj.ads_disabled = true;
+obj.subscription_type = "premium";
+obj.premium_until = 4070908800; // 2099年
+obj.features = ["all", "no_ads", "high_res", "fast_update"];
+
+// 针对部分版本可能存在的嵌套结构
+if (obj.data) {
+    obj.data.is_premium = true;
+    obj.data.premium_until = 4070908800;
+}
 
 $done({body: JSON.stringify(obj)});
